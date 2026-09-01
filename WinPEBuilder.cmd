44e66f684f0564165ff792ed1938c86c102a1fc7
susaimumbai-oss <susai.mumbai@gmail.com>
2026-09-02 00:25:52 +0530

    Translate error messages and prompts to English
    Translated error messages and prompts from Chinese to English for better accessibility.

--- WinPEBuilder.cmd
+++ WinPEBuilder.cmd
@@ -57,7 +57,7 @@ if not "x%APP_SRC_FOLDER%"=="x" (
   ) else if exist "%APP_SRC_FOLDER%\sources\install.esd" (
     set "APP_SRC=%APP_SRC_FOLDER%\sources\install.esd"
   ) else (
-    echo 错误：在 %APP_SRC_FOLDER% 中找不到源镜像
+    echo Error：in %APP_SRC_FOLDER% The source image not found
     goto :EOF
   )
 
@@ -69,29 +69,29 @@ if not "x%APP_SRC_FOLDER%"=="x" (
       set "APP_BASE_WIM=%APP_SRC_FOLDER:~,1%:\sources\winre.wim"
       set APP_BASE_INDEX=1
     ) else (
-      echo 错误：在 %APP_SRC_FOLDER% 中找不到 boot.wim 或 winre.wim
+      echo Error：in %APP_SRC_FOLDER% Not found boot.wim or winre.wim
       goto :EOF
     )
   )
 ) else if not "x%APP_SRC_WIM%"=="x" (
-  if not exist "%APP_SRC_WIM%" (echo 错误：源WIM文件不存在：%APP_SRC_WIM% & goto :EOF)
+  if not exist "%APP_SRC_WIM%" (echo Error：The source WIM file does not exist：%APP_SRC_WIM% & goto :EOF)
   set "APP_SRC=%APP_SRC_WIM%"
 ) else (
-  echo 错误：必须指定 --source-folder 或 --source-wim 参数
+  echo Error：Must be slected --source-folder or --source-wim Parameters
   goto :EOF
 )
 
-if "x%APP_BASE_WIM%"=="x" (echo 错误：未指定基础镜像 & goto :EOF)
-if "x%APP_OPT_PROJECT%"=="x" (echo 错误：未指定项目 & goto :EOF)
+if "x%APP_BASE_WIM%"=="x" (echo Error：No base image selected & goto :EOF)
+if "x%APP_OPT_PROJECT%"=="x" (echo Error：Project not selected & goto :EOF)
 
-if not exist "%APP_BASE_WIM%" (echo 错误：基础WIM文件不存在：%APP_BASE_WIM% & goto :EOF)
-if not exist "%cd%\projects\%APP_OPT_PROJECT%" (echo 错误：项目不存在：%APP_OPT_PROJECT% & goto :EOF)
+if not exist "%APP_BASE_WIM%" (echo Error：Base WIM file not exist：%APP_BASE_WIM% & goto :EOF)
+if not exist "%cd%\projects\%APP_OPT_PROJECT%" (echo Error：The project does not exist：%APP_OPT_PROJECT% & goto :EOF)
 
 if "x%APP_SRC_INDEX%"=="x" set APP_SRC_INDEX=1
 if "x%APP_BASE_INDEX%"=="x" set APP_BASE_INDEX=1
 
-echo [命令行] 源镜像：%APP_SRC%，索引：%APP_SRC_INDEX%
-echo [命令行] 基础镜像：%APP_BASE_WIM%，索引：%APP_BASE_INDEX%
+echo [Command Line] Source Image：%APP_SRC%，Index：%APP_SRC_INDEX%
+echo [Command Line] Base Image：%APP_BASE_WIM%，Index：%APP_BASE_INDEX%
 
 set "APP_PROJECT=%APP_OPT_PROJECT%"
 set "APP_BASE_PATH=%cd%\target\%APP_PROJECT%\base.wim"
@@ -100,21 +100,21 @@ copy /y "%APP_BASE_WIM%" "%cd%\target\%APP_PROJECT%\base.wim" >nul
 goto :build
 
 :cui
-set /p APP_SRC_FOLDER=输入虚拟光驱盘符或目录路径：
-if "%APP_SRC_FOLDER%"=="" (echo 没有输入虚拟光驱盘符或目录路径 & goto :cui)
+set /p APP_SRC_FOLDER=Enter ISO mounted drive letter or directory path：
+if "%APP_SRC_FOLDER%"=="" (echo No drive letter or directory path was entered & goto :cui)
 if "%APP_SRC_FOLDER:~1%"=="" set "APP_SRC_FOLDER=%APP_SRC_FOLDER%:"
 if exist "%APP_SRC_FOLDER%\sources\install.wim" (
   set "APP_SRC=%APP_SRC_FOLDER%\sources\install.wim"
 ) else if exist "%APP_SRC_FOLDER%\sources\install.esd" (
   set "APP_SRC=%APP_SRC_FOLDER%\sources\install.esd"
 ) else (
-  echo 没找到 install.wim 和 install.esd
+  echo Not found install.wim or install.esd
   goto :cui
 )
 echo ————————————————————————————————————————————————
 for /f "tokens=2 delims=: " %%a in ('Dism.exe /English /Get-WimInfo /WimFile:"%APP_SRC%" ^| findstr /i Index') do (
   Dism.exe /English /Get-WimInfo /WimFile:"%APP_SRC%" /Index:%%a >"%TEMP%\wiminfo_%%a.tmp" 2>nul
-  rem 转换为UTF-8编码
+  rem Convert to UTF-8 encoding
   iconv -f GBK -t UTF-8 "%TEMP%\wiminfo_%%a.tmp" > "%TEMP%\wiminfo_%%a_utf8.tmp" 2>nul
   del "%TEMP%\wiminfo_%%a.tmp" 2>nul
 
@@ -128,24 +128,24 @@ for /f "tokens=2 delims=: " %%a in ('Dism.exe /English /Get-WimInfo /WimFile:"%A
   set index=%%a
 )
 echo ————————————————————————————————————————————————
-set /p APP_SRC_INDEX=输入install分卷号：
-if "%APP_SRC_INDEX%"=="" (set APP_SRC_INDEX=1&echo 没有输入分卷号，已自动选择卷1)
-if %APP_SRC_INDEX% lss 1 (echo 没有选择对应的分卷号 & goto :cui)
-if %APP_SRC_INDEX% gtr %index% (echo 没有选择对应的分卷号 & goto :cui)
+set /p APP_SRC_INDEX=Enter Index Volume Number：
+if "%APP_SRC_INDEX%"=="" (set APP_SRC_INDEX=1&echo No Volume number is entered，Volume 1 is auto selected)
+if %APP_SRC_INDEX% lss 1 (echo No volume number was selected & goto :cui)
+if %APP_SRC_INDEX% gtr %index% (echo No volume number was selected & goto :cui)
 
 set index=0
 echo.
-echo 已有的WinPE项目：
+echo Existing WinPE Projects：
 echo ————————————————————————————————————————————————
 for /d %%i in ("%cd%\Projects\*") do (
   set /a index=index+1
   echo  !index!     %%~nxi
 )
 echo ————————————————————————————————————————————————
-set /p project=请输入项目序号：
-if "%project%"=="" (echo 没有选择项目 & goto :cui)
-if "%project%" lss "1" (echo 没有选择对应的项目 & goto :cui)
-if "%project%" gtr "%index%" (echo 没有选择对应的项目 & goto :cui)
+set /p project=Enter Project Number：
+if "%project%"=="" (echo Select None & goto :cui)
+if "%project%" lss "1" (echo No project selected & goto :cui)
+if "%project%" gtr "%index%" (echo No project selected & goto :cui)
 set index=0
 for /d %%x in ("%cd%\Projects\*") do (
   set /a index=index+1
@@ -153,17 +153,17 @@ for /d %%x in ("%cd%\Projects\*") do (
 )
 
 echo.
-echo 基础镜像
+echo Base Image
 echo ————————————————————————————————————————————————
-echo  1     Boot.wim (卷2) (适用于Boot修订号同步的镜像)
-echo  2     WinRE.wim (卷1) (适用于WinRE修订号同步的镜像)
-echo        自定义基础镜像 (卷1)
+echo  1     Boot.wim (Volume2) (Base to Boot WinPE)
+echo  2     WinRE.wim (Volume1) (Base to WinRE WinPE)
+echo        Custom Base Image (Volume1)
 echo ————————————————————————————————————————————————
-set /p bore=请输入基础镜像：
-if "%bore%"=="" (set bore=1&echo 没有选择 wim,已自动选择 Boot.wim)
+set /p bore=Please Enter Base Image:
+if "%bore%"=="" (set bore=1&echo Not selected wim, Auto selected Boot.wim)
 
 echo.
-choice /M "是否制作ISO镜像？"
+choice /M "Create an ISO image?"
 if %errorlevel%==1 set APP_OPT_MAKE_ISO=1
 if %errorlevel%==2 set APP_OPT_MAKE_ISO=0
 
@@ -176,20 +176,20 @@ if "%bore%"=="1" (
   if not exist "%cd%\target\%APP_PROJECT%" mkdir "%cd%\target\%APP_PROJECT%"
   set "APP_BASE_PATH=%cd%\target\%APP_PROJECT%\winre.wim"
   set APP_BASE_INDEX=1
-  echo \033[93;46m [构建] 提取winre.wim...... | CmdColor.exe
+  echo \033[93;46m [Build] Extracting winre.wim...... | CmdColor.exe
   wimlib-imagex.exe extract "%APP_SRC%" %APP_SRC_INDEX% "\Windows\System32\Recovery\winre.wim" --dest-dir="%cd%\target\%APP_PROJECT%" --nullglob --no-acls
 ) else if exist "%bore%" (
   if not exist "%cd%\target\%APP_PROJECT%" mkdir "%cd%\target\%APP_PROJECT%"
   copy /y "%bore%" "%cd%\target\%APP_PROJECT%\base.wim" >nul
   set "APP_BASE_PATH=%cd%\target\%APP_PROJECT%\base.wim"
   set APP_BASE_INDEX=1
-) else echo 没有选择对应的项目 & goto :cui
+) else echo No project selected & goto :cui
 
 :build
 set "t=%time: =0%"
 set /a "start_ts=(1%t:~0,2%-100)*3600 + (1%t:~3,2%-100)*60 + (1%t:~6,2%-100)"
 
-echo \033[93;46m [构建] 获取基础镜像信息...... | CmdColor.exe
+echo \033[93;46m [Build] Getting base image details... | CmdColor.exe
 for /f "tokens=1,2 delims=:(" %%i in ('Dism.exe /Get-WimInfo /WimFile:"%APP_BASE_PATH%" /Index:%APP_BASE_INDEX% /English') do (
   if "%%i"=="Architecture " set APP_PE_ARCH=%%j
   if "%%i"=="Version " set APP_PE_VER=%%j
@@ -201,7 +201,7 @@ for /f "tokens=1,2 delims=:(" %%i in ('Dism.exe /Get-WimInfo /WimFile:"%APP_BASE
   if "%%i"=="Languages " set LANG_FLAG=1
 )
 if "x%APP_PE_LANG%"=="x" (
-  echo \033[93;46m [构建] 获取基础镜像信息失败 | CmdColor.exe
+  echo \033[93;46m [Build] Failed to get base image details | CmdColor.exe
   if "x%APP_EXEC_MODE%"=="x1" goto :EOF
   cmd /k
 )
@@ -239,29 +239,29 @@ call V2X -init
 if not exist "%X%" mkdir "%X%"
 if not exist "%APP_TMP_PATH%" mkdir "%APP_TMP_PATH%"
 
-echo \033[93;46m [构建] 提取安装镜像注册表...... | CmdColor.exe
+echo \033[93;46m [Build] Extracting install image registry... | CmdColor.exe
 wimlib-imagex.exe extract "%APP_SRC%" %APP_SRC_INDEX% "\Windows\System32\config\DEFAULT" --dest-dir="%SRC_PATH%" --nullglob --no-acls --preserve-dir-struct
 wimlib-imagex.exe extract "%APP_SRC%" %APP_SRC_INDEX% "\Windows\System32\config\DRIVERS" --dest-dir="%SRC_PATH%" --nullglob --no-acls --preserve-dir-struct
 wimlib-imagex.exe extract "%APP_SRC%" %APP_SRC_INDEX% "\Windows\System32\config\SYSTEM" --dest-dir="%SRC_PATH%" --nullglob --no-acls --preserve-dir-struct
 wimlib-imagex.exe extract "%APP_SRC%" %APP_SRC_INDEX% "\Windows\System32\config\SOFTWARE" --dest-dir="%SRC_PATH%" --nullglob --no-acls --preserve-dir-struct
 wimlib-imagex.exe extract "%APP_SRC%" %APP_SRC_INDEX% "\Users\Default\NTUSER.DAT" --dest-dir="%SRC_PATH%" --nullglob --no-acls --preserve-dir-struct
 
-rem 挂载基础镜像
+rem Mount base image
 call RunHooks before-mount.cmd
-echo \033[93;46m [构建] 挂载基础镜像: %APP_BASE_PATH% | CmdColor.exe
+echo \033[93;46m [Build] Mount base image: %APP_BASE_PATH% | CmdColor.exe
 Dism /mount-wim /wimfile:"%APP_BASE_PATH%" /index:%APP_BASE_INDEX% /mountdir:"%X%"
 call RunHooks after-mount.cmd
 
 rem 挂载注册表
 call RunHooks before-mount-reg.cmd
-echo \033[93;46m [构建] 挂载安装镜像镜像注册表 | CmdColor.exe
+echo \033[93;46m [Setup] Mount install image and setup | CmdColor.exe
 reg load HKLM\Src_SOFTWARE "%SRC_PATH%\Windows\System32\config\SOFTWARE"
 reg load HKLM\Src_SYSTEM "%SRC_PATH%\Windows\System32\config\SYSTEM"
 reg load HKLM\Src_DEFAULT "%SRC_PATH%\Windows\System32\config\DEFAULT"
 reg load HKLM\Src_DRIVERS "%SRC_PATH%\Windows\System32\config\DRIVERS"
 reg load HKLM\Src_NTUSER.DAT "%SRC_PATH%\Users\Default\NTUSER.DAT"
 
-echo \033[93;46m [构建] 挂载基础镜像注册表 | CmdColor.exe
+echo \033[93;46m [Build] Mount base image registry | CmdColor.exe
 reg load HKLM\Tmp_SOFTWARE "%X%\Windows\System32\config\SOFTWARE"
 reg load HKLM\Tmp_SYSTEM "%X%\Windows\System32\config\SYSTEM"
 reg load HKLM\Tmp_DEFAULT "%X%\Windows\System32\config\DEFAULT"
@@ -270,13 +270,13 @@ reg load HKLM\Tmp_NTUSER.DAT "%X%\Users\Default\NTUSER.DAT"
 
 call RunHooks after-mount-reg.cmd
 
-rem 执行项目脚本
+rem Run the project script
 call RunHooks before-project.cmd
 call ApplyProjectPatches "%PROJECT_PATH%"
 call RunHooks after-project.cmd
-echo \033[93;46m [构建] 项目执行完成，正在清理...... | CmdColor.exe
+echo \033[93;46m [Build] Project operation completed; cleaning up..... | CmdColor.exe
 
-rem 卸载注册表
+rem Remove registry keys
 reg unload HKLM\Src_SOFTWARE
 reg unload HKLM\Src_SYSTEM
 reg unload HKLM\Src_DEFAULT
@@ -289,13 +289,13 @@ reg unload HKLM\Tmp_DEFAULT
 reg unload HKLM\Tmp_DRIVERS
 reg unload HKLM\Tmp_NTUSER.DAT
 
-rem 压缩注册表
+rem Compress registry
 ru.exe -accepteula -nobanner -h "%X%\Windows\System32\config\DRIVERS"
 ru.exe -accepteula -nobanner -h "%X%\Windows\System32\config\SOFTWARE"
 ru.exe -accepteula -nobanner -h "%X%\Windows\System32\config\SYSTEM"
 ru.exe -accepteula -nobanner -h "%X%\Users\Default\NTUSER.DAT"
 
-rem 清理注册表日志文件
+rem Clean up registry log files
 del /f /q /a "%X%\Windows\System32\config\*.LOG*" 1>nul 2>nul
 del /f /q /a "%X%\Windows\System32\config\*{*}*" 1>nul 2>nul
 del /f /q /a "%X%\Windows\System32\SMI\Store\Machine\*.LOG*" 1>nul 2>nul
@@ -303,24 +303,24 @@ del /f /q /a "%X%\Windows\System32\SMIStore\Machine\*{*}*" 1>nul 2>nul
 del /f /q /a "%X%\Users\Default\*.LOG*" 1>nul 2>nul
 del /f /q /a "%X%\Users\Default\*{*}*" 1>nul 2>nul
 
-rem 保存并卸载卷
+rem Save and unmount volume
 call RunHooks before-commit.cmd
-echo \033[93;46m [构建] 正在卸载基础镜像...... | CmdColor.exe
+echo \033[93;46m [Build] Saving Unmount base image...... | CmdColor.exe
 Dism /Unmount-Image /MountDir:"%X%" /commit
 
-rem 导出镜像
-echo \033[93;46m [构建] 正在导出WinPE镜像...... | CmdColor.exe
+rem Export image
+echo \033[93;46m [Build] Exporting WinPE image...... | CmdColor.exe
 if exist "%BUILD_WIM%" del /A /F /Q "%BUILD_WIM%"
 rem wimlib-imagex.exe export "%APP_BASE_PATH%" %APP_BASE_INDEX% "%BUILD_WIM%" --boot
 Dism /Export-Image /SourceImageFile:"%APP_BASE_PATH%" /SourceIndex:%APP_BASE_INDEX% /DestinationImageFile:"%BUILD_WIM%" /Bootable
 
-rem 构建ISO镜像
+rem Build an ISO image
 if "x%APP_OPT_MAKE_ISO%"=="x1" (
-  echo \033[93;46m [构建] 正在构建ISO镜像...... | CmdColor.exe
+  echo \033[93;46m [Build] Build ISO image...... | CmdColor.exe
   call MakeBootISO.cmd
 )
 
-rem 清理文件
+rem Clean up files
 rd /s /q "%SRC_PATH%" 2>nul
 rd /s /q "%X%" 2>nul
 rd /s /q "%APP_TMP_PATH%" 2>nul
@@ -334,9 +334,9 @@ set /a "diff=end_ts - start_ts"
 if %diff% LSS 0 set /a "diff+=86400"
 set /a "min=diff / 60, sec=diff %% 60"
 if %min% GTR 0 (
-  echo \033[93;46m [完成] WinPE制作完成，总耗时%min%分%sec%秒. | CmdColor.exe
+  echo \033[93;46m [Complete] WinPE creation complete; total time taken %min% minutes %sec% seconds. | CmdColor.exe
 ) else (
-  echo \033[93;46m [完成] WinPE制作完成，总耗时%sec%秒. | CmdColor.exe
+  echo \033[93;46m [Complete] WinPE creation complete; total time: %sec% seconds. | CmdColor.exe
 )
 
 call RunHooks finished.cmd
