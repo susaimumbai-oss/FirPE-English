919a4fc120192698969cd33400583638584c9fac
susaimumbai-oss <susai.mumbai@gmail.com>
2026-09-02 05:46:46 +0530

    Change language resources from zh-CN to en-US

--- Projects/WinPE/00_Simplify/04_精简WinSxS.cmd
+++ Projects/WinPE/00_Simplify/04_精简WinSxS.cmd
@@ -4,7 +4,7 @@ call AddFiles %0 :end_files
 goto :end_files
 
 ; //- Language without fallback language should be enough for WinSxS
-\Windows\WinSxS\amd64_microsoft.windows.c..-controls.resources_*_zh-CN*\*.*
+\Windows\WinSxS\amd64_microsoft.windows.c..-controls.resources_*_en-US*\*.*
 \Windows\WinSxS\amd64_microsoft.windows.common-controls*\*.*
 \Windows\WinSxS\amd64_microsoft.windows.gdiplus.systemcopy_*\*.*
 \Windows\WinSxS\amd64_microsoft.windows.gdiplus_*\*.*
@@ -12,15 +12,15 @@ goto :end_files
 \Windows\WinSxS\amd64_microsoft.windows.i..utomation.proxystub_*\*.*
 \Windows\WinSxS\amd64_microsoft-windows-servicingstack_*\*.*
 ; //-
-\Windows\WinSxS\manifests\amd64_microsoft.windows.c..-controls.resources_*_zh-CN*.manifest
+\Windows\WinSxS\manifests\amd64_microsoft.windows.c..-controls.resources_*_en-US*.manifest
 \Windows\WinSxS\manifests\amd64_microsoft.windows.common-controls*.manifest
 \Windows\WinSxS\manifests\amd64_microsoft.windows.gdiplus.systemcopy_*.manifest
 \Windows\WinSxS\manifests\amd64_microsoft.windows.gdiplus_*.manifest
 \Windows\WinSxS\manifests\amd64_microsoft.windows.isolationautomation_*.manifest
 \Windows\WinSxS\manifests\amd64_microsoft.windows.i..utomation.proxystub_*.manifest
 \Windows\WinSxS\manifests\amd64_microsoft-windows-comdlg32_*.manifest
-\Windows\WinSxS\manifests\amd64_microsoft-windows-comctl32-v5.resources_*_zh-CN*.manifest
-\Windows\WinSxS\manifests\amd64_microsoft-windows-comdlg32.resources_*_zh-CN*.manifest
+\Windows\WinSxS\manifests\amd64_microsoft-windows-comctl32-v5.resources_*_en-US*.manifest
+\Windows\WinSxS\manifests\amd64_microsoft-windows-comdlg32.resources_*_en-US*.manifest
 \Windows\WinSxS\manifests\amd64_microsoft.windows.systemcompatible_*.manifest
 \Windows\WinSxS\manifests\amd64_microsoft-windows-a..core-base.resources_*.manifest
 \Windows\WinSxS\manifests\amd64_microsoft-windows-blb-engine-main_*.manifest
